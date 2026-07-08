b420237a6c98f91d9b65474ffd850da1f46fdfdb
Lipi Makwana <lipi@Lipis-MacBook-Air.local>
2026-07-09 00:39:29 +0530

    Setup Flask configuration and home route

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
