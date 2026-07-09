d439690c9642f049628cd4401c322320ade7266e
Lipi Makwana <lipi@Lipis-MacBook-Air.local>
2026-07-09 15:05:01 +0530

    Create dashboard page structure

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
