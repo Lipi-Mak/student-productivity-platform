057d4127423ce60c931f7abd5feaf7da8c6b7edf
Lipi Makwana <lipi@Lipis-MacBook-Air.local>
2026-07-12 23:46:06 +0530

    Implement user authentication backend with Flask-Login

--- models.py
+++ models.py
@@ -1,13 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
 
     username = db.Column(db.String(100), unique=True, nullable=False)
 
     email = db.Column(db.String(120), unique=True, nullable=False)
 
-    password = db.Column(db.String(200), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
