826a7204465c6811d9eef68fd9d7b321bd232d5e
Lipi Makwana <lipi@Lipis-MacBook-Air.local>
2026-07-12 13:44:45 +0530

    Pushing new ui, phase 3.5

--- app.py
+++ app.py
@@ -4,9 +4,21 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+@app.route("/")
+def home():
+    return redirect(url_for("login"))
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
