c6d6f3b987e304dba9f37c9cc66c6db2683f0d3f
Lipi Makwana <lipi@Lipis-MacBook-Air.local>
2026-07-16 14:28:13 +0530

    Prepare StudentOS for Render deployment

--- config.py
+++ config.py
@@ -3,8 +3,14 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "studentproductivitysecret"
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "studentproductivitysecret"
+    )
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///students.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "students.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
